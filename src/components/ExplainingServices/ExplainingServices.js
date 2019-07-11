85bc0156b48f0080c50bd4d8c802e38c0217053c
Hackerrr23 <yusefdaramay@yahoo.com>
2019-07-11 17:53:14 -0500

    services half done

---
+++ src/components/ExplainingServices/ExplainingServices.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./ExplainingServices.scss";
+export default function ExplainingServices() {
+  return (
+    <div>
+      <div className="explaining">
+        <h1 className="orange">Delivering Experiences That Matter</h1>
+        <div className="explain-para">
+          <p>
+            projekt202 is the #1 choice for the world’s most successful
+            companies looking to transform their customer and employee
+            experiences. Why? It’s in our unique,{" "}
+            <span className="orange">established methodology</span> for
+            understanding people in context — we reveal your customers’ unmet
+            needs, wants and motivations — which drives everything we do. This
+            leads to a genuine, clear understanding of the customer, which
+            shapes the <span className="orange">design and developmen</span>t of
+            new software solutions and experiences.
+          </p>
+        </div>
+        <div className="explain-para">
+          <p>
+            With over 15 years perfecting our approach, we have the experience,
+            <span className="orange">teams</span>, skills and scale to deliver
+            sophisticated software that improves any and all touchpoints across
+            the user journey.
+          </p>
+        </div>
+        <div className="explain-para">
+          <p>
+            Looking to improve a product, deliver a new workplace solution, or
+            disrupt your industry? Look no further than projekt202 for today's
+            experience-driven transformation opportunities.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,41 +1,48 @@
 import React from "react";
 import "./Landing.scss";
+
+//files
+import ExplainingServices from "../ExplainingServices/ExplainingServices";
+
 export default function Landing() {
   return (
-    <div id="landing">
-      <div className="under">
-        <div className="color-overlay" />
-        <header>
-          <h2 id="logo">
-            projekt<span className="twotwo">202</span>
-          </h2>
-          <nav>
-            <ul>
-              <li>WHY PROJEKT202?</li>
-              <li>PRACTICES</li>
-              <li>METHODOLOGY</li>
-              <li>PEOPLE</li>
-              <li>NEWS & INSIGHTS</li>
-              <li>JOIN US</li>
-              <li id="contact-us">
-                <span id="contact">CONTACT US </span>
-              </li>
-            </ul>
-          </nav>
-        </header>
+    <div>
+      <div id="landing">
+        <div className="under">
+          <div className="color-overlay" />
+          <header>
+            <h2 id="logo">
+              projekt<span className="twotwo">202</span>
+            </h2>
+            <nav>
+              <ul>
+                <li>WHY PROJEKT202?</li>
+                <li>PRACTICES</li>
+                <li>METHODOLOGY</li>
+                <li>PEOPLE</li>
+                <li>NEWS & INSIGHTS</li>
+                <li>JOIN US</li>
+                <li id="contact-us">
+                  <span id="contact">CONTACT US </span>
+                </li>
+              </ul>
+            </nav>
+          </header>
+        </div>
+        <main>
+          <div id="middle">
+            <p id="site-desc">
+              <strong>experience-driven transformation</strong>
+            </p>
+          </div>
+          <div id="services">
+            {" "}
+            <p>Strategy |</p> <p>UX Design |</p> <p>Development |</p>{" "}
+            <p>Marketing</p>
+          </div>
+        </main>
       </div>
-      <main>
-        <div id="middle">
-          <p id="site-desc">
-            <strong>experience-driven transformation</strong>
-          </p>
-        </div>
-        <div id="services">
-          {" "}
-          <p>Strategy |</p> <p>UX Design |</p> <p>Development |</p>{" "}
-          <p>Marketing</p>
-        </div>
-      </main>
+      <ExplainingServices />
     </div>
   );
 }
